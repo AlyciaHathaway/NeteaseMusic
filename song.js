491c9c43069a5e3b43da517ede729e3a5b781cfa
Alycia <alyciahathaways@gmail.com>
2018-05-31 16:28:24 +0800

    音乐播放

--- song.js
+++ song.js
@@ -17,5 +17,12 @@ $(function() {
 			$p.attr('data-time', object.time).text(object.words);
 			$p.appendTo($lyric.children('.lines'));
 		})
-	})
-})
+	});
+
+	let audio = document.createElement('audio');
+	audio.src = 'http://dl.stream.qqmusic.qq.com/C400001pTnkI06GIsr.m4a?vkey=6AD20A0BBE5B1AB3A8DEBB445B0B8FCE93D451F0F4581594371BF0AFC566F0FF9431B2846E9CBD7FBA33292488C3D9CDDFA7EBCCA79FCE34&guid=2580314012&uin=0&fromtag=66';
+	audio.oncanplay = function() {
+		audio.play();
+		$('.disc-container').addClass('playing');
+	}
+});
